517f833b5fad9f9dd6cc7e9df6b1dbc1c67c28c0
root <secondcycle@proton.me>
2022-11-07 13:08:40 -0800

    token transfer functioning now

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import Profile from "./Profile"
 import SwitchNetwork from "./SwitchNetwork";
+import "../App.css";
+import "../index.css";
 
 const Header = () => {
     return (
         <>
-        <h1>token.taxi</h1>
+        <h1 className="text-3xl font-bold underline">token.taxi</h1>
         <Profile />
         {/* <SwitchNetwork /> */}
         </>
