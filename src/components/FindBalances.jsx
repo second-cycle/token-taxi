3e9be0df74f91db31843ad51f9d9b61298cb14bd
root <secondcycle@proton.me>
2022-11-13 09:50:17 -0800

    typo

--- src/components/FindBalances.jsx
+++ src/components/FindBalances.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const FindBalances = async () => {
-    const address = "0x7fd9626768D02F61da6754EC7B0382FeA8110781";
+    const address = "";
     const etherscanCall = `https://api.etherscan.io/api?module=account&action=tokentx&address=${address}&apikey=IVIC44B3R4T44RPR7WQ4SGUV1AMYH89GH1`;
     const response = await axios.get(etherscanCall);
 
